2ce8d9ba7280e7ca20abdc8ab9ae345b7fd2df8e
Nonon <iipocto1deagle@gmail.com>
2020-10-25 21:26:04 +0300

    formatting

--- src/View/main_template.html.php
+++ src/View/main_template.html.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>On-line курсы GeekStars | <?= $title ?? '' ?></title>
 </head>
+
 <body>
     <table width="100%">
         <tr>
@@ -14,26 +16,45 @@
                     <tr valign="top">
                         <td width="150">
                             <table cellspacing="10">
-                                <tr><td><a href="<?= $path('index') ?>">Главная</a></td></tr>
-                                <tr><td><a href="<?= $path('product_list') ?>">Товары</a></td></tr>
-                                <tr><td><a href="<?= $path('product_desc_list') ?>">Список товаров</a></td></tr>
-                                <tr><td><a href="<?= $path('order_info') ?>">Корзина</a></td></tr>
+                                <tr>
+                                    <td><a href="<?= $path('index') ?>">Главная</a></td>
+                                </tr>
+                                <tr>
+                                    <td><a href="<?= $path('product_list') ?>">Товары</a></td>
+                                </tr>
+                                <tr>
+                                    <td><a href="<?= $path('product_desc_list') ?>">Список товаров</a></td>
+                                </tr>
+                                <tr>
+                                    <td><a href="<?= $path('order_info') ?>">Корзина</a></td>
+                                </tr>
                                 <?php if (!$isAuth) {
-    ?>
-                                    <tr><td><a href="<?= $path('user_authentication') ?>">Авторизация</a></td></tr>
+                                ?>
+                                    <tr>
+                                        <td><a href="<?= $path('user_authentication') ?>">Авторизация</a></td>
+                                    </tr>
+                                    <?php
+                                } else {
+                                    if ($isAdmin) {
+                                    ?>
+                                        <tr>
+                                            <td><a href="<?= $path('user_list') ?>">Все пользователи</a></td>
+                                        </tr>
+                                    <?php
+                                    } ?>
+                                    <tr>
+                                        <td><a href="<?= $path('logout') ?>">Выход</a></td>
+                                    </tr>
                                 <?php
-} else {
-        ?>
-                                    <tr><td><a href="<?= $path('logout') ?>">Выход</a></td></tr>
-                                <?php
-    } ?>
+                                } ?>
                             </table>
                         </td>
-                        <td align="center"><br/><?= isset($body) && $body instanceof Closure ? $body() : '' ?><br/></td>
+                        <td align="center"><br /><?= isset($body) && $body instanceof Closure ? $body() : '' ?><br /></td>
                     </tr>
                 </table>
             </td>
         </tr>
     </table>
 </body>
+
 </html>
